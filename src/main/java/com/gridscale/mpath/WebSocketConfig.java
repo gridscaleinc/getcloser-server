2b3fdf41f18bda15061d8125ae38e26c9361db09
domanthan <domanthan@gmail.com>
2020-05-01 15:47:58 +0900

    change websocket config

--- src/main/java/com/gridscale/mpath/WebSocketConfig.java
+++ src/main/java/com/gridscale/mpath/WebSocketConfig.java
@@ -78,5 +78,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/endpoint-getcloser").withSockJS();
+		registry.addEndpoint("/endpoint-getcloser").setAllowedOrigins("*");
 	}
 }
